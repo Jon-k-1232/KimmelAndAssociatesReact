903da58cd4297773b0fcfb86b38b5d101dd03631
Jon Kimmel <Jonkimmel16@gmail.com>
2022-08-22 14:37:36 -0700

    add retirement articles.

--- src/Components/ArticleContainers/ArticleContainer.js
+++ src/Components/ArticleContainers/ArticleContainer.js
@@ -4,7 +4,7 @@ export default function ArticleContainer({ file }) {
   return (
     <div className='articleContainer'>
       <a className='articleATag' href={file.file} target='_blank' rel='noreferrer'>
-        <img src={file.image} alt={file.alt} />
+        <img style={{ borderRadius: '9px' }} src={file.image} alt={file.alt} />
         <h3 id='fileDisplayName'>{file.displayName}</h3>
       </a>
     </div>
--- src/Resources/ResourceFileEnum.js
+++ src/Resources/ResourceFileEnum.js
@@ -1,8 +1,17 @@
 import inflationReductionAct from './ResourceFiles/Inflation_Reduction_Act.pdf';
 import taxGuide from './ResourceFiles/2022_Tax_Guide.pdf';
+import sCorpAndCcorpRetirementPlans2022 from './ResourceFiles/S_Corp_and_C_Corp Retirement_Plans.pdf';
+import soleProprietorshipAndPartnershipRetirementPlans2022 from './ResourceFiles/Sole_Proprietorship_and_Partnership_Retirement_Plans.pdf';
+import estateAndTrustTaxGuide2022 from './ResourceFiles/estate_And_Trust_Tax_Guide_2022.pdf';
 import inflation from '../images/inflation.jpeg';
 import bookKeep from '../images/bookkeeping.jpeg';
+import glassesAndEstate from '../images/glassesAndEstate.png';
+import retirement from '../images/retirement.png';
 
+/**
+ * Catagories:
+ * featured, individual, business, estatesAndTrusts, retirementPlanning, independentContractors
+ */
 const files = [
   {
     file: inflationReductionAct,
@@ -11,10 +20,28 @@ const files = [
     displayName: 'Inflation Reduction Act of 2022',
     group: 'featured'
   },
-  { file: taxGuide, image: bookKeep, alt: 'Tax Guide', displayName: '2022 Tax Guide', group: 'featured' }
-  // { file: 'test B1', image: '', alt:'', displayName: '',  group: 'Business' },
-  // { file: 'test B2', image: '', alt:'', displayName: '',  group: 'Business' },
-  // { file: 'test C1', image: '', alt:'', displayName: '',  group: 'EstatesAndTrusts' },
+  { file: taxGuide, image: bookKeep, alt: 'Tax Guide', displayName: '2022 Tax Guide', group: 'featured' },
+  {
+    file: sCorpAndCcorpRetirementPlans2022,
+    image: retirement,
+    alt: 'Tax Guide',
+    displayName: 'S Corp and C Corp Retirement Plans 2022',
+    group: 'retirementPlanning'
+  },
+  {
+    file: soleProprietorshipAndPartnershipRetirementPlans2022,
+    image: retirement,
+    alt: 'Tax Guide',
+    displayName: 'Sole Proprietorship and Partnership Retirement Plans 2022',
+    group: 'retirementPlanning'
+  },
+  {
+    file: estateAndTrustTaxGuide2022,
+    image: glassesAndEstate,
+    alt: 'Tax Guide',
+    displayName: 'Estate and Trust Tax Guide 2022',
+    group: 'estatesAndTrusts'
+  }
   // { file: 'test C2', image: '', alt:'', displayName: '',  group: 'EstatesAndTrusts' },
   // { file: 'test D1', image: '', alt:'', displayName: '',  group: 'RetirementPlanning' },
   // { file: 'test D2', image: '', alt:'', displayName: '',  group: 'RetirementPlanning' }
