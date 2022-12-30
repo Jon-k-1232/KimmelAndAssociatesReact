85ecfba6051a313d0f7703b28776a4cb20a5964e
Jon Kimmel <Jonkimmel16@gmail.com>
2022-12-30 11:12:03 -0700

    more resources

--- src/Resources/ResourceFileEnum.js
+++ src/Resources/ResourceFileEnum.js
@@ -13,6 +13,18 @@ import giftTax2022 from './ResourceFiles/giftTax2022.pdf';
 import divorce2022 from './ResourceFiles/divorce2022.pdf';
 import audits2022 from './ResourceFiles/audits2022.pdf';
 import estimatedTaxes2022 from './ResourceFiles/estimatedTaxes2022.pdf';
+import taxFacts2022 from './ResourceFiles/taxFacts2022.pdf';
+import employeeOrContractor2022 from './ResourceFiles/employeeOrContractor2022.pdf';
+import sCorporations2022 from './ResourceFiles/sCorporations2022.pdf';
+import principalResidenceSale2022 from './ResourceFiles/principalResidenceSale2022.pdf';
+import estatePlanning from './ResourceFiles/estatePlanning.pdf';
+import businessOwners2022 from './ResourceFiles/businessOwners2022.pdf';
+import individualIncomeTaxExtension2022 from './ResourceFiles/individualIncomeTaxExtension2022.pdf';
+import familiesAndChildren2022 from './ResourceFiles/familiesAndChildren2022.pdf';
+import businessEntityComparison2022 from './ResourceFiles/businessEntityComparison2022.pdf';
+import irsNotice2022 from './ResourceFiles/irsNotice2022.pdf';
+import cantPayTaxes2022 from './ResourceFiles/cantPayTaxes2022.pdf';
+import reverseMortgages2022 from './ResourceFiles/reverseMortgages2022.pdf';
 import highIncome from '../images/highIncome.png';
 import giftTax from '../images/giftTax.png';
 import divorce from '../images/divorce.png';
@@ -27,6 +39,18 @@ import glassesAndEstate from '../images/glassesAndEstate.png';
 import retirement from '../images/retirement.png';
 import identity from '../images/identity.png';
 import traditionalIRAs from '../images/traditionalIRAs.png';
+import taxFacts2022Img from '../images/taxFacts2022Img.png';
+import employeeOrContractor2022Img from '../images/employeeOrContractor2022Img.png';
+import sCorporations2022Img from '../images/sCorporations2022Img.png';
+import principalResidenceSale2022Img from '../images/principalResidenceSale2022Img.png';
+import estatePlanningImg from '../images/estatePlanningImg.png';
+import businessOwners2022Img from '../images/businessOwners2022Img.png';
+import individualIncomeTaxExtension2022Img from '../images/individualIncomeTaxExtension2022Img.png';
+import familiesAndChildren2022Img from '../images/familiesAndChildren2022Img.png';
+import businessEntityComparison2022Img from '../images/businessEntityComparison2022Img.png';
+import irsNotice2022Img from '../images/irsNotice2022Img.png';
+import cantPayTaxes2022Img from '../images/cantPayTaxes2022Img.png';
+import reverseMortgages2022Img from '../images/reverseMortgages2022Img.png';
 
 /**
  * Catagories:
@@ -67,7 +91,7 @@ const files = [
     image: identity,
     alt: 'identity Theft',
     displayName: 'Identity Theft And Your Taxes',
-    group: 'individual'
+    group: 'featured'
   },
   { file: traditionalIra, image: traditionalIRAs, alt: 'Traditional IRA', displayName: 'Traditional IRAs', group: 'retirementPlanning' },
   { file: taxpayerDeath2022, image: taxPayerDeath, alt: 'asset', displayName: 'Taxpayer Death 2022', group: 'estatesAndTrusts' },
@@ -94,14 +118,105 @@ const files = [
   },
   { file: giftTax2022, image: giftTax, alt: 'Gift Tax 2022', displayName: 'Gift Tax 2022', group: 'individual' },
   { file: divorce2022, image: divorce, alt: 'Divorce 2022', displayName: 'Divorce 2022', group: 'individual' },
-  { file: audits2022, image: audits, alt: 'Audits 2022', displayName: 'Audits 2022', group: 'individual' },
-  { file: estimatedTaxes2022, image: estimatedTaxes, alt: 'Estimated Taxes 2022', displayName: 'Estimated Taxes 2022', group: 'individual' }
+  { file: audits2022, image: audits, alt: 'Audits 2022', displayName: 'Audits 2022', group: 'featured' },
+  {
+    file: estimatedTaxes2022,
+    image: estimatedTaxes,
+    alt: 'Estimated Taxes 2022',
+    displayName: 'Estimated Taxes 2022',
+    group: 'business'
+  },
+  //TODO
+  {
+    file: taxFacts2022,
+    image: taxFacts2022Img,
+    alt: 'Tax Facts 2022',
+    displayName: 'Tax Facts 2022',
+    group: 'featured'
+  },
+  {
+    file: employeeOrContractor2022,
+    image: employeeOrContractor2022Img,
+    alt: 'Employee Or Contractor 2022',
+    displayName: 'Employee Or Contractor 2022',
+    group: 'independentContractors'
+  },
+  {
+    file: sCorporations2022,
+    image: sCorporations2022Img,
+    alt: 'S Corporations 2022',
+    displayName: 'S Corporations 2022',
+    group: 'business'
+  },
+  {
+    file: principalResidenceSale2022,
+    image: principalResidenceSale2022Img,
+    alt: 'Sale of a Principal Residence 2022',
+    displayName: 'Sale of a Principal Residence 2022',
+    group: 'individual'
+  },
+  {
+    file: estatePlanning,
+    image: estatePlanningImg,
+    alt: 'Estate Planning, Wills, Probate, and Transfer of Assets 2022',
+    displayName: 'Estate Planning, Wills, Probate, and Transfer of Assets 2022',
+    group: 'retirementPlanning'
+  },
+  {
+    file: businessOwners2022,
+    image: businessOwners2022Img,
+    alt: 'Business Owners—Taking Money Out of a Business 2022',
+    displayName: 'Business Owners—Taking Money Out of a Business 2022',
+    group: 'business'
+  },
+  {
+    file: individualIncomeTaxExtension2022,
+    image: individualIncomeTaxExtension2022Img,
+    alt: 'Extending Your Individual Income Tax Return 2022',
+    displayName: 'Extending Your Individual Income Tax Return 2022',
+    group: 'featured'
+  },
+  {
+    file: familiesAndChildren2022,
+    image: familiesAndChildren2022Img,
+    alt: 'Families With Children 2022',
+    displayName: 'Families With Children 2022',
+    group: 'individual'
+  },
+  {
+    file: businessEntityComparison2022,
+    image: businessEntityComparison2022Img,
+    alt: 'Business Entity Comparison Chart 2022',
+    displayName: 'Business Entity Comparison Chart 2022',
+    group: 'business'
+  },
+  {
+    file: irsNotice2022,
+    image: irsNotice2022Img,
+    alt: 'Taxpayers Who Receive an IRS Notice 2022',
+    displayName: 'Taxpayers Who Receive an IRS Notice 2022',
+    group: 'featured'
+  },
+  {
+    file: cantPayTaxes2022,
+    image: cantPayTaxes2022Img,
+    alt: 'What To Do if You Can’t Pay Your Taxes 2022',
+    displayName: 'What To Do if You Can’t Pay Your Taxes 2022',
+    group: 'featured'
+  },
+  {
+    file: reverseMortgages2022,
+    image: reverseMortgages2022Img,
+    alt: 'Reverse Mortgages 2022',
+    displayName: 'Reverse Mortgages 2022',
+    group: 'individual'
+  }
 
-  // { file: 'test C2', image: '', alt:'', displayName: '',  group: 'EstatesAndTrusts' },
-  // { file: 'test D1', image: '', alt:'', displayName: '',  group: 'RetirementPlanning' },
-  // { file: 'test D2', image: '', alt:'', displayName: '',  group: 'RetirementPlanning' }
-  // { file: 'test E1', image: '', alt:'', displayName: '',  group: 'IndependentContractors' },
-  // { file: 'test E2', image: '', alt:'', displayName: '',  group: 'IndependentContractors' }
+  // { file: 'test C2', image: '', alt:'', displayName: '',  group: 'estatesAndTrusts' },
+  // { file: 'test D1', image: '', alt:'', displayName: '',  group: 'retirementPlanning' },
+  // { file: 'test D2', image: '', alt:'', displayName: '',  group: 'retirementPlanning' }
+  // { file: 'test E1', image: '', alt:'', displayName: '',  group: 'independentContractors' },
+  // { file: 'test E2', image: '', alt:'', displayName: '',  group: 'independentContractors' }
 ];
 
 export default files;
